38e58f3f8524653e9c3498ccaaf96217c60ba1e4
nikhilmohan <nikmohan81@gmail.com>
2020-10-25 21:55:46 +0530

    Initial commit

---
+++ src/main/java/com/nikhilm/hourglass/moviefactory/MovieFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilm.hourglass.moviefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikhilm/hourglass/moviefactory/MovieFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilm.hourglass.moviefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
